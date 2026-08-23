7fc68131ecf13e92a27c9e07420abc56b68bd76a
imashwaaa <imashwaaa@gmail.com>
2026-08-23 22:29:12 +0530

    Add relational operators

--- Basics/operators.c
+++ Basics/operators.c
@@ -11,7 +11,14 @@ int main() {
     int c=2;
     printf("Pre-increment: %d\n",++c);
     printf("Post-increment: %d\n",c++);
-    printf("After-increment: %d",c);
+    printf("After-increment: %d\n",c);
+
+    //relational operators
+    int k=2;
+    printf("%d\n",a>b);//output 1
+    printf("%d\n",a<b);//output 0
+    printf("%d\n",c==k);//output 0(bcz c value changed after increment)
+    printf("%d\n",a!=b);//output 1
 
     return 0;
 }
